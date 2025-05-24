a083c2f754fba1e812ef1eecc51184e5095d50e8
Catherine <c.n@pdx.edu>
2025-05-24 13:31:02 -0700

    Midi is now supported :DDD

---
+++ midi_handler.gd
@@ -0,0 +1,21 @@
+extends Node
+
+func _ready():
+	OS.open_midi_inputs()
+	print(OS.get_connected_midi_inputs())
+
+func _input(input_event):
+	if input_event is InputEventMIDI:
+		if input_event.pitch != 0:
+			_print_midi_info(input_event)
+
+func _print_midi_info(midi_event):
+	print(midi_event)
+	print("Channel ", midi_event.channel)
+	print("Message ", midi_event.message)
+	print("Pitch ", midi_event.pitch)
+	print("Velocity ", midi_event.velocity)
+	print("Instrument ", midi_event.instrument)
+	print("Pressure ", midi_event.pressure)
+	print("Controller number: ", midi_event.controller_number)
+	print("Controller value: ", midi_event.controller_value)
